f76bcf36d3ff90cba87a7ad25f12306cdb3e2bf2
Tikuwa404 <mashidori2@gmail.com>
2024-12-15 15:03:04 +0900

    fix:fitFromCSVの入力部の修正、6段階評価を実装

--- fitFromCSV.py
+++ fitFromCSV.py
@@ -7,11 +7,12 @@ from tensorflow.keras import layers, models
 # 出力：どのような姿勢か
 
 # 学習用入力データのサイズ
-SIZE_ROW = 3
+SIZE_ROW = 39
 SIZE_COL = 9
 
 x_train = numpy.ndarray(shape=[SIZE_ROW, SIZE_COL])
-y_train = numpy.ndarray(shape=[SIZE_ROW])
+class_train = numpy.ndarray(shape=[SIZE_ROW])
+goodness_train = numpy.ndarray(shape=[SIZE_ROW])
 
 # CSVファイル「sensor_data.csv」からセンサーデータを取得
 with open("sensor_data.csv", encoding="utf8") as f:
@@ -24,7 +25,7 @@ with open("sensor_data.csv", encoding="utf8") as f:
             if j[0] == "\ufeff":
                 j = j[1:]
 
-            x_train[idx_row, idx_col] = int(j)
+            x_train[idx_row, idx_col] = float(j)
             idx_col = idx_col + 1
         idx_row = idx_row + 1
 
@@ -34,30 +35,64 @@ with open("class_data.csv", encoding="utf8") as f:
     idx_row = 0
     for i in reader:
         # \ufeffを除去
-        if i[0] == "\ufeff":
-            j = j[1:]
+        if i[0][0] == "\ufeff":
+            i[0][0] = i[0][1:]
 
-        y_train[idx_row] = int(j)
+        class_train[idx_row] = int(i[0])
         idx_row = idx_row + 1
 
 # モデルの構築
-model = models.Sequential([
-    layers.Input(shape=(9,)),                 # 入力層（6つの特徴量）
+class_model = models.Sequential([
+    layers.Input(shape=(9,)),                 # 入力層（9つの特徴量）
     layers.Dense(64, activation='relu'),      # 隠れ層1
     layers.Dense(32, activation='relu'),      # 隠れ層2
     layers.Dense(6, activation='softmax')     # 出力層（6クラス分類）
 ])
 
 # モデルのコンパイル
-model.compile(optimizer='adam',
+class_model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
 # モデルの概要を表示
-model.summary()
+# class_model.summary()
 
 # モデルを学習
-model.fit(x_train, y_train, epochs=10, batch_size=32)
+class_model.fit(x_train, class_train, epochs=10, batch_size=32)
 
 # モデルの出力
-model.save("bogosort_model.keras")
+class_model.save("bogosort_class.keras")
+
+# CSVファイル「goodness_data.csv」から良さデータを取得
+with open("goodness_data.csv", encoding="utf8") as f:
+    reader = csv.reader(f)
+    idx_row = 0
+    for i in reader:
+        # \ufeffを除去
+        if i[0][0] == "\ufeff":
+            i[0][0] = i[0][1:]
+
+        goodness_train[idx_row] = int(i[0])
+        idx_row = idx_row + 1
+
+# モデルの構築
+goodness_model = models.Sequential([
+    layers.Input(shape=(9,)),                 # 入力層（9つの特徴量）
+    layers.Dense(64, activation='relu'),      # 隠れ層1
+    layers.Dense(32, activation='relu'),      # 隠れ層2
+    layers.Dense(6, activation='softmax')     # 出力層（6クラス分類）
+])
+
+# モデルのコンパイル
+goodness_model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+# モデルの概要を表示
+# goodness_model.summary()
+
+# モデルを学習
+goodness_model.fit(x_train, goodness_train, epochs=10, batch_size=32)
+
+# モデルの出力
+goodness_model.save("bogosort_goodness.keras")
